025d7ac8d4be7db0dc9f6a28eeb6130f206367fb
Kamil <florczykkamil@gmail.com>
2018-05-11 23:32:48 +0200

    Init project

---
+++ src/test/java/pl/kflorczyk/lrucache/LrucacheApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kflorczyk.lrucache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LrucacheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
